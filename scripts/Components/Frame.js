fb6a247c0cc373c4f5d4d6b31a00fce33bceaae3
thundoon <thundoon@gmail.com>
2016-03-12 20:56:42 -0800

    fix css|

--- scripts/Components/Frame.js
+++ scripts/Components/Frame.js
@@ -1,11 +1,8 @@
 // Need this for the css styles to be included
-import '../styles/Header.css'
+import '../../styles/styles.css'
 // need this to create a proper react component
 import React, {Component} from 'react';
 
-var divStyle = {
-	color:'blue'
-}
 
 
 // when Import 'App' is called, this object is exported
@@ -14,7 +11,7 @@ export default class Frame extends Component {
   render() {
     return (
       // Add your component markup and other subcomponent references here.
-      <p className = "Header"	>Hello, World!</p>
+      <p className = "frame "	>Hello, World!</p> 
     );
   }
 }
