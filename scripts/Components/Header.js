e7f220b26a1232985d82dd9174311d9098761f65
thundoon <thundoon@gmail.com>
2016-03-14 22:15:11 -0700

    Missed a file

---
+++ scripts/Components/Header.js
@@ -0,0 +1,13 @@
+import '../../styles/styles.css'
+import React, {Component} from 'react';
+
+
+export default class Header extends Component {
+	render() {
+		return (
+				<div className = "header">
+					<h1 className = "title">Personal Backlog</h1>
+				</div>
+			)
+	}
+}
