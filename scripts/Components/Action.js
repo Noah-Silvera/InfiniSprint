7e0017631646f5a14ad3aef4a892b2f4a4e148cc
thundoon <thundoon@gmail.com>
2016-03-25 09:07:03 -0700

    Experimenting with event model and props

--- scripts/Components/Action.js
+++ scripts/Components/Action.js
@@ -1,21 +1,30 @@
+'use strict'
 import '../../styles/styles.css'
 import React, {Component} from 'react';
 
 
+// STATELESS
+// this class renders an action item from google calender
+// When dragged over a valid drop target, it emits an onDragOverEvent
 export default class Action extends Component {
 
 	render() {
-		console.log(this.props.rank)
+		console.log(this.props.rank)	
 		return (
-				<p className = "action" draggable = "true" onDragOver = {this.onDragOver}>
-						{this.props.content}
-				</p>
+				<div className = "action" draggable = "true" onDragOver = {this.onDragOver.bind(this, this.props)}>
+						<p className = "row">{this.props.rank} -- </p><p className = "row">{this.props.content}</p>
+				</div>
 			)
 
 	}
 
 	onDragOver = (e) => {
-		console.log("dragged over a valid drop target");
+		// console.log("dragged over" + e.target.innerH	TML);
+		console.log("props = " + this.props.content.toString())
+		for( var p in this.props ) {
+			console.log( p.toString() )
+		}
+
 
 		// Prevents a browsers default behaviour ( like navigating to a link )
 		if (e.preventDefault) {
